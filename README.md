8d7eb0c21366376efd3835acc3b335ab9f197186
iassadki <ilias.assadki@gmail.com>
2024-05-06 11:35:20 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# powerpack-webtoapp
+
+## 💻 Description
+PowerPack WebToApp is a WordPress plugin designed to extend the functionality of PowerPack by allowing manipulation of content for display on both web browsers and mobile applications.
+
+## 🛠 About the project
+This reposotory is contains hooks to modify OS display options in PowerPack.
+
+## ⚙️ Functionalities
+- Adds custom hooks to modify OS display options in PowerPack.
+- Provides options for displaying content on web browsers and mobile applications.
+
+## 🛠 Installation
+1. Download the plugin zip file.
+2. Log in to your WordPress dashboard.
+3. Navigate to Plugins > Add New.
+4. Click on the "Upload Plugin" button, then select the plugin zip file.
+5. Activate the plugin.
+
+## 🛠 Technologies
+Languages : PHP, Wordpress
+
+## 🛠 Usage
+Once activated, the plugin automatically adds custom hooks to modify OS display options in PowerPack. You can then utilize these options to control the display of content on web browsers and mobile applications.
+
+## 🛠 Licence
+This plugin is licensed under the GNU General Public License v2.0.
---
+++ powerpack-webtoapp.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: PowerPack WebToApp
+Version: 1.0
+Description: Extension qui permet de manipuler du contenu en l'affichant sur navigateur ou application mobile
+Author: Ilias Assadki -- PowerPack
+*/
+ 
+// Add a custom hook to modify the OS display options in PowerPack
+function powerpack_webtoapp_custom_hook() {
+    // Adds custom options for OS
+    add_filter( 'ppe_os_display_condition_options', 'powerpack_webtoapp_custom_os_options_filter', 10, 1 );
+    add_filter( 'ppe_os_display_condition_check', 'powerpack_webtoapp_custom_os_options_check_filter', 10, 1 );
+}
+
+// Filter OS display options for PowerPack
+function powerpack_webtoapp_custom_os_options_filter( $oses ) {
+    // Add your custom option
+    $oses['mobile_app'] = 'App-WebView';
+    return $oses;
+}
+
+// Filter OS check options for PowerPack
+function powerpack_webtoapp_custom_os_options_check_filter( $oses ) {
+    // Ajoute ton option personnalisée
+    $oses['mobile_app'] = 'App-WebView';
+    return $oses;
+}
+
+// Add hooks when WordPress is initialized
+add_action( 'init', 'powerpack_webtoapp_custom_hook' );
+
+
